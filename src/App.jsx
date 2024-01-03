8726ce430ca9f6caaefa0fc17b0a72f3cb6b849f
kasivisu4 <kasivisu4@gmail.com>
2024-01-02 17:22:42 -0800

    Intialized vite and react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./globals.css";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      Welcome to Feedback Buddy
+    </div>
+  );
+};
+
+export default App;
